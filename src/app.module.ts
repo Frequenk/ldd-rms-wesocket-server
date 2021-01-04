15968f2a2a69466cd40441144ed9e151cfff5ee1
fuyikun <1281363146@qq.com>
2021-01-05 00:26:29 +0800

    ➕feature(cache):添加redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { ArticleController } from './article/article.controller';
 import { NewsService } from './news/news.service';
 import { EventsGateway } from './events.gateway';
+import { RedisModule } from 'nestjs-redis';
+import { CacheService } from './cache.service';
 
+const options = {
+  port: 6379,
+  host: 'rms.leiduoduo.top',
+  password: '',
+  db: 0,
+};
 @Module({
-  imports: [],
+  imports: [RedisModule.register(options)],
   controllers: [AppController, ArticleController],
-  providers: [AppService, NewsService, EventsGateway],
+  providers: [AppService, NewsService, EventsGateway, CacheService],
 })
 export class AppModule {}
